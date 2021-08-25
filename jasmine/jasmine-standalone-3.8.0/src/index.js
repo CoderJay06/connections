7255ce7cc54b7b0aef245b3d9f84c99471fb0c2c
CoderJay06 <jaycru06@yahoo.com>
2021-08-25 14:17:34 -0400

    Switch from localStorage to indexedDB, remove uneeded files

--- jasmine/jasmine-standalone-3.8.0/src/index.js
+++ jasmine/jasmine-standalone-3.8.0/src/index.js
@@ -10,18 +10,21 @@
         - handleSave()
 
  */
+let connectionsDb;
 
 document.addEventListener("DOMContentLoaded", () => {
     // let connectionsFromLocalStorage = [];
     let connections = [];
-    const connectionsFromLocalStorage = getConnections();
+    // const connectionsFromLocalStorage = getConnections();
+    const removeBtn = document.querySelector(".remove-btn");
+    setupConnectionsDb();
      // append connections to dom as a list
-     if (connectionsFromLocalStorage) {
-        console.log('c ', connections)
-        console.log('c from ls ', connectionsFromLocalStorage)
-        connections = connectionsFromLocalStorage;
-        render(connections);
-    }
+    //  if (connectionsFromLocalStorage) {
+    //     removeBtn.style.display = "inline-block";
+    //     connections = connectionsDb;
+    //     render(connections);
+    // }
+    // removeConnectionsOnClick(connections);
     saveConnectionsOnClick(connections);
     console.log('DomLoaded')
 });
@@ -62,25 +65,48 @@ function getButton() {
 
 function saveConnectionsOnClick(connections) {
     const saveBtn = getButton();
-
+    const removeBtn = document.querySelector(".remove-btn");
     // handle saving connection
-    saveBtn.addEventListener("click", () => {
+    saveBtn.addEventListener("click", (e) => {
+        e.preventDefault();
         // get user inputs from form
         const inputs = getInputs();
         const nameInput = inputs.getName();
         const emailInput = inputs.getEmail();
         const phoneInput = inputs.getPhone();
 
-        // store inputs in an array
-        connections.push({
+        const newConnection = {
             "name": nameInput.value,
             "email": emailInput.value,
             "phone": phoneInput.value
-        });
+        };
+
+        // store inputs in an array
+        connections.push(newConnection);
         console.log('connections ', connections)
+        // // save inputs in localStorage
+        // localStorage.setItem("connections", JSON.stringify(connections));
+        saveConnectionsToDb(newConnection);
         resetForm(inputs);
-        // save inputs in localStorage
-        localStorage.setItem("connections", JSON.stringify(connections));
+        removeBtn.style.display = "inline-block";
+        render(connections);
+    });
+}
+
+function removeConnectionsOnClick(connections) {
+    const removeBtn = document.querySelector(".remove-btn");
+    // add event listener for remove button
+    removeBtn.addEventListener("dblclick", (e) => {
+        e.preventDefault();
+        removeBtn.style.display = "none";
+
+        // remove connections from localStorage
+        localStorage.removeItem("connections");
+
+        // reset connections array
+        connections = [];
+
+        // render empty array
         render(connections);
     });
 }
@@ -89,6 +115,65 @@ function resetForm(inputs) {
     inputs.getName().value = "";
     inputs.getEmail().value = "";
     inputs.getPhone().value = "";
+}
+
+function setupConnectionsDb() {
+    window.onload = () => {
+        let request = window.indexedDB.open("connections", 1);
+
+        request.onerror = () => {
+            alert("Connections not loaded");
+        };
+
+        request.onsuccess = () => {
+            alert("Successfully loaded connections");
+
+            connectionsDb = request.result;
+        };
+
+        request.onupgradeneeded = (e) => {
+            
+            // grab reference to db
+            let db = e.target.result;
+
+            let objectStore = db.createObjectStore(
+                "connections", { keyPath: "id", autoIncrement: true}
+            );
+
+            objectStore.createIndex("name", "name", { unique: false });
+            objectStore.createIndex("email", "email", { unique: false });
+            objectStore.createIndex("phone", "phone", { unique: false });
+        }
+
+    };
+}
+
+function saveConnectionsToDb(newConnection) {
+    let connection = newConnection;
+    console.log('db in saveConnectionsToDb ', connectionsDb)
+    console.log('newConnection ', connection);
+
+    let transaction = connectionsDb.transaction(["connections"], "readwrite");
+
+    let objectStore = transaction.objectStore("connections");
+
+    let request = objectStore.add({
+        name: connection.name,
+        email: connection.email,
+        phone: connection.phone
+    });
+
+    request.onsuccess = () => {
+        console.log(`Successfully stored ${connection.name} to db`);
+    }
+
+    transaction.oncomplete = () => {
+        console.log("Transaction completed");
+    }
+
+    transaction.onerror = () => {
+        console.log("Transaction not completed. Error");
+    }
 }
 
 function render(connections) {
